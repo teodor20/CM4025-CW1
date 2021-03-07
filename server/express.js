3c7aad6ca051857203c120cb0ff0e426b2722276
DESKTOP-B6JLE13\test <teodor.narliyski@gmail.com>
2021-03-07 17:09:43 +0000

    Added client-side code - authorization and private routing, user-api and test home page (coming from lab6)

--- server/express.js
+++ server/express.js
@@ -7,11 +7,29 @@ import cors from 'cors'
 import helmet from 'helmet'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
+import theme from './../client/theme'
+
 
 //TODO: Delete later
 import Template from './../template'
 
+//TODO: comment out before building for production
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
+//TODO: comment out before building for production
+devBundle.compile(app)
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -45,8 +63,25 @@ app.use((err, req, res, next) => {
 })
 
 //TODO: Change Template to something else
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
-})
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets()
+    const context = {}
+    const markup = ReactDOMServer.renderToString(
+        sheets.collect( <StaticRouter location={req.url} context={context}>
+            <ThemeProvider theme={theme}>
+            <MainRouter />
+            </ThemeProvider>
+            </StaticRouter>
+        )
+        )
+    if (context.url) {
+        return res.redirect(303, context.url)
+    }
+    const css = sheets.toString()
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+    }))
+   })
 
 export default app
