c26c7b692b9ed0cac27208a2b8049f590fc58264
DESKTOP-B6JLE13\test <teodor.narliyski@gmail.com>
2021-03-11 15:55:34 +0000

    Fix for hot loader not working

--- client/App.js
+++ client/App.js
@@ -1,9 +1,9 @@
+import { hot } from 'react-hot-loader'
 import React from 'react'
 import MainRouter from './MainRouter'
 import {BrowserRouter} from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme'
-import { hot } from 'react-hot-loader'
 const App = () => {
     
 React.useEffect(() => {
