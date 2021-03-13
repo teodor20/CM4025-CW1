72a89b05967feefa41285fb107e421416734b84f
DESKTOP-B6JLE13\test <teodor.narliyski@gmail.com>
2021-03-13 12:47:53 +0000

    Included source map to enable api debugging

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -5,6 +5,7 @@ const CURRENT_WORKING_DIR = process.cwd()
 const config = {
     name: "server",
     entry: [ path.join(CURRENT_WORKING_DIR , './server/server.js') ],
+    devtool: 'cheap-module-source-map',
     target: "node",
     output: {
         path: path.join(CURRENT_WORKING_DIR , '/dist/'),
