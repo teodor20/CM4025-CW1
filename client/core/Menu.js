f2f5a20fa34a9d5876b87245e4cb1c457855a01f
DESKTOP-B6JLE13\test <teodor.narliyski@gmail.com>
2021-03-15 23:09:08 +0000

    Moved the navbar buttons to the right

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -2,10 +2,12 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
 import auth from '../auth/auth-helper'
 import {Link, withRouter} from 'react-router-dom'
 import logo from './../assets/images/logo.png'
 import { makeStyles } from '@material-ui/core/styles';
+import theme from '../theme'
 
 const isActive = (history, path) => {
   if (history.location.pathname == path)
@@ -47,18 +49,18 @@ let brand = <img src={ content.brand.image } alt="" width={ content['brand'].wid
 const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
-      {/* <Link to="/" variant="h5" color="inherit" underline="none" className={classes.linkBrand}> */}
-      <Link to="/" variant="h5" color="inherit" underline="none">
-        {brand}
-      </Link>
+      <Box display='flex' flexGrow={1}>
+        <Link to="/" variant="h5" color="inherit" underline="none">
+          {brand}
+        </Link>
+      </Box>
       {
         !auth.isAuthenticated() && (<span>
           <Link to="/signin">
-            {/* <Button color="inherit" style={isActive(history, "/signup")} className={classes.secondaryButton}>{content['secondary-action']} </Button> */}
-            <Button color="inherit" style={isActive(history, "/signin")} >Sign In </Button>
+            <Button color="inherit" style={{float:'left', ...isActive(history, "/signin")}} >Sign In </Button>
           </Link>
           <Link to="/signup">
-            <Button variant="contained" color="secondary" style={isActive(history, "/signup")}>Sign Up</Button>
+            <Button variant="contained" color="secondary" style={{float:'left', ...isActive(history, "/signup")}}>Sign Up</Button>
           </Link>
         </span>)
       }
