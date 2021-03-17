654c3d3c47551a58ba7f59ff6b13bf360ab94657
DESKTOP-B6JLE13\test <teodor.narliyski@gmail.com>
2021-03-17 23:56:50 +0000

    Add chart to the user profile showing how many games of each type they have played

--- client/components/user/Profile.js
+++ client/components/user/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import { Pie } from 'react-chartjs-2';
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -15,6 +16,7 @@ import Divider from '@material-ui/core/Divider'
 import DeleteUser from './DeleteUser'
 import auth from '../../auth/auth-helper'
 import {read} from '../api/api-user.js'
+import {getUserGames} from '../api/api-game.js'
 import {Redirect, Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
@@ -35,6 +37,50 @@ export default function Profile({ match }) {
   const [user, setUser] = useState({})
   const [redirectToSignin, setRedirectToSignin] = useState(false)
   const jwt = auth.isAuthenticated()
+  const [data, setData] = useState({
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Classic Snake',
+        'Tic-Tac-Toe',
+        '2048'
+    ]
+
+    
+  })
+
+  const populateUserGames = () => {
+    getUserGames(
+      {userId: match.params.userId},
+      {t: jwt.token}).then((data) => {
+        if (data && data.error) {
+        } else {
+          //Count the different types of games
+          let snakeGames = 0;
+          let tttGames = 0;
+          let puzzleGames = 0;
+
+          data.forEach(game => {
+            if (game.type == "CS") {snakeGames++}
+            else if (game.type == "TTT") {tttGames++}
+            else {puzzleGames++}
+          })
+        
+          let dataArray = [snakeGames, tttGames, puzzleGames]
+
+          setData({datasets: [{
+              data: dataArray,
+              backgroundColor: [
+                '#FFAEBC',
+                '#A0E7E5',
+                '#B4F8C8'
+              ]
+            }]
+          })
+
+        }
+      })
+  }
 
   useEffect(() => {
     const abortController = new AbortController()
@@ -46,7 +92,8 @@ export default function Profile({ match }) {
       if (data && data.error) {
         setRedirectToSignin(true)
       } else {
-        setUser(data)
+        setUser(data);
+        populateUserGames();
       }
     })
 
@@ -56,39 +103,42 @@ export default function Profile({ match }) {
 
   }, [match.params.userId])
   
-    if (redirectToSignin) {
-      return <Redirect to='/signin'/>
-    }
-    return (
-      <Paper className={classes.root} elevation={4}>
-        <Typography variant="h6" className={classes.title}>
-          Profile
-        </Typography>
-        <List dense>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <Person/>
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={user.name} secondary={user.email}/> {
-             auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
-              (<ListItemSecondaryAction>
-                <Link to={"/user/edit/" + user._id}>
-                  <IconButton aria-label="Edit" color="primary">
-                    <Edit/>
-                  </IconButton>
-                </Link>
-                <DeleteUser userId={user._id}/>
-              </ListItemSecondaryAction>)
-            }
-          </ListItem>
-          <Divider/>
-          <ListItem>
-            <ListItemText primary={"Joined: " + (
-              new Date(user.created)).toDateString()}/>
-          </ListItem>
-        </List>
-      </Paper>
-    )
+  if (redirectToSignin) {
+    return <Redirect to='/signin'/>
   }
+
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        Profile
+      </Typography>
+      <List dense>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Person/>
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.name} secondary={user.email}/> {
+           auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
+            (<ListItemSecondaryAction>
+              <Link to={"/user/edit/" + user._id}>
+                <IconButton aria-label="Edit" color="primary">
+                  <Edit/>
+                </IconButton>
+              </Link>
+              <DeleteUser userId={user._id}/>
+            </ListItemSecondaryAction>)
+          }
+        </ListItem>
+        <Divider/>
+        <ListItem>
+          <ListItemText primary={"Joined: " + (
+            new Date(user.created)).toDateString()}/>
+        </ListItem>
+      </List>
+      <Typography variant="h6" className={classes.title}>Games played:</Typography>
+      <Pie data={data} id="test" options="area" />
+    </Paper>
+  )
+}
